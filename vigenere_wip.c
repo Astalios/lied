26ef37896c1edcb0e7d35eaadf2b0695b9e37441
Astalios <simon.debrou974@gmail.com>
2019-03-22 16:03:40 +0100

    Vigenere fonctionnel / corrections a faire

--- vigenere_wip.c
+++ vigenere_wip.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <ctype.h>
-// le ft_viegenere est simple, on lui injecte deux donnees, le texte en premier, et la valeur de decalage, positive ou negative, il gere les majuscules et les minuscules. 
+// A modifier , faire en sorte que ce soit un vrai vigenere, car a + a = m  au lieu de a + a = b 
 char	ft_vigenere(char input[] , char increment[])
 	{
 		int j=0;
@@ -19,10 +19,10 @@ char	ft_vigenere(char input[] , char increment[])
 				if (input[i] >= 'A'  && input[i] <= 'Z')
 				{
 					if (input[i] + increment[j] < 'A')
-						input[i] = (((input[i] + 25 + increment[j]) % 26) + 65);
+						input[i] = (((input[i] + 25 + increment[j]) % 27) + 65);
 					else if (input[i] + increment[j] > 'Z')
-						input[i] = (((input[i] - 25 + increment[j]) % 26) + 65);
-					else input[i] = (((input[i] + increment[j]) % 26) + 65);
+						input[i] = (((input[i] - 25 + increment[j]) % 27) + 65);
+					else input[i] = (((input[i] + increment[j]) % 27) + 65);
 				}
 				else if (input[i] >= 'a' && input[i] <= 'z')
 				{
@@ -43,13 +43,10 @@ char	ft_vigenere(char input[] , char increment[])
 		printf("\n");
 		return(0);
 	}
-
-// Creating main, protected to avoid segfaults and some basic mis-input ( more than two ) a simplifier, à faire : utiliser les ocmmandes de capture clavier, 
-// premices de la CLI que j ai en tete la premiere condition ne fonctionne pas
 int	main(int argc, char** argv)
 	{
 		if (argc != 3)
-			printf("il ne faut rentrer que deux valeurs \n" /* ou tappez './cesar --help' pour plus d'informations."*/);
+			printf("il faut rentrer deux valeurs \n");
 		else	
 			ft_vigenere(argv[1], argv[2]);
 		return(0);
