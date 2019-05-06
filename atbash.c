1b352ed90f5c3d7dfe6353d2e1fccae0267222c2
naeya <tbretech@protonmail.com>
2019-05-06 09:40:59 +0200

    Add atbash and temporary main

---
+++ atbash.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char atbash(char cipher[], char key[27], char decipher[])
+	{
+		int a, b;
+		int len = strlen(key);
+
+		for (a=0; cipher[a] != 0; a++)
+			{
+				for (b=0; key[b] != 0; b++)
+					{
+						if (cipher[a] == key[b])
+							{
+								decipher[a] = key[len - b - 1];
+							}
+					}
+			}
+	}
+
+int main(int argc, char *argv[])
+	{
+		char output[strlen(argv[1]+1)];
+
+		atbash(argv[1], "abcdefghijklmnopqrstuvwxyz", output);
+		printf("%s\n", output);
+		return 0;
+	}
--- substitutionWIP.c
+++ substitutionWIP.c
@@ -2,28 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-char substitution (char cipher[], char key[27], char *decipher)
+char substitution (char cipher[], char key1[27], char key2[27], char decipher[])
   {
-    char alph[27] = "abcdefghijklmnopqrstuvwxyz";
     int a, b;
 
     for (a=0; cipher[a] != 0; a++)
       {
-        for(b=0; key[b] != 0; b++)
+        for(b=0; key1[b] != 0; b++)
           {
-            if(cipher[a] == key[b])
+            if(cipher[a] == key1[b])
               {
-                decipher[a] = alph[b];
+                decipher[a] = key2[b];
               }
           }
       }
-
   }
 
 
 int main(int argc, char *argv[])
   {
-    char decipher[sizeof(argv[1])];
-    substitution(argv[1], argv[2], decipher);
-    printf("%s\n", decipher);
+
+		if(argc != 4)
+			{
+				printf("Wrong input\n");
+				return 0;
+			}
+		else
+			{
+    		char output[sizeof(argv[1])];
+    		substitution(argv[1], argv[2], argv[3], output);
+    		printf("%s\n", output);
+    		return 0;
+    	}
+
   }
